e9016bd7050a3f919838decb3ea0fc5c04d069a5
terry <terry@qq.com>
2025-05-22 11:17:48 +0800

    添加手机号码保护函数SecurePhone

--- utils/utils.go
+++ utils/utils.go
@@ -16,6 +16,17 @@ func IsPhone(phone string) bool {
 	return reg.MatchString(phone)
 }
 
+// 手机号码隐私保护
+func SecurePhone(phone string) string {
+	// 匹配中国大陆11位手机号
+	re := regexp.MustCompile(`1[3-9]\d{9}`)
+
+	return re.ReplaceAllStringFunc(phone, func(match string) string {
+		// 隐藏中间四位数字
+		return match[:3] + "****" + match[7:]
+	})
+}
+
 // GenCode 生成安全的验证码,codeType 为 n,s,m 分别代表数字，字母，数字和字母混合；len 表示生成的验证码长度
 func GenCode(codeType string, codeLen int) string {
 	var charset string
